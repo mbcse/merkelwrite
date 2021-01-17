014bd7d99ea6bcccf0ac80e44f69b9e1eb298bab
mbcse <mbcse50@gmail.com>
2021-01-18 02:33:07 +0530

    Final Contract

--- Solidity/bedium.sol
+++ Solidity/bedium.sol
@@ -26,8 +26,8 @@ interface CompoundErc20 {
 
 contract bedium{
     
-    daiErc20 dai=daiErc20(0x4F96Fe3b7A6Cf9725f59d353F723c1bDb64CA6Aa);
-    CompoundErc20 compound=CompoundErc20(0xF0d0EB522cfa50B716B3b1604C4F0fA6f04376AD);
+    daiErc20 dai=daiErc20(0xfe4F5145f6e09952a5ba9e956ED0C25e3Fa4c7F1);
+    CompoundErc20 compound=CompoundErc20(0x822397d9a55d0fefd20F5c4bCaB33C5F65bd28Eb);
     
     uint totalPost;
     address owner;
@@ -144,8 +144,8 @@ contract bedium{
     function transferDai(uint _amount) public returns(bool){
         dai.transferFrom(msg.sender,address(this),_amount);
         subscriptionPeriod[msg.sender]=block.timestamp+30 days;
-        dai.approve(address(compound), _amount);
-        uint mintResult = compound.mint(_amount);
+        // dai.approve(address(compound), _amount);
+        // uint mintResult = compound.mint(_amount);
         return true;
     }   
 
